f5aa3aaf56d2b663f0d1a139c3af96bd178ac219
Ivan <ivanyeremin12@gmail.com>
2025-01-27 15:16:46 +0200

    init

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,54 @@
+import { ThemeProvider } from "@orc/web/components/theme-provider";
+import { Toaster } from "@orc/web/components/ui/sonner";
+import { cn } from "@orc/web/lib/utils";
+import type { Metadata, Viewport } from 'next';
+import { Inter as FontSans } from "next/font/google";
+import "./globals.css";
+import { ThemeToggle } from '@orc/web/components/theme-toggle';
+import { TailwindIndicator } from '@orc/web/components/tailwind-indicator';
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+export const metadata: Metadata = {
+  title: "Magic UI",
+  description: "The startup template from Magic UI",
+};
+
+export const viewport: Viewport = {
+  colorScheme: 'dark',
+  themeColor: [
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          disableTransitionOnChange
+          enableSystem={false}
+        >
+          {children}
+          <Toaster />
+          <ThemeToggle />
+          <TailwindIndicator />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ apps/web/components/theme-toggle.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { Button } from './ui/button';
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
+import { useEffect } from 'react';
+
+export function ThemeToggle() {
+  const { setTheme, theme } = useTheme();
+  // Don't show in production
+  // if (process.env.NODE_ENV === 'production') return null;
+  useEffect(() => {
+    console.log('theme', theme);
+  }, [theme]);
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      className="fixed bottom-1 left-1 z-50"
+      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+    >
+      <Sun className="h-[1.5rem] w-[1.3rem] dark:hidden" />
+      <Moon className="hidden h-5 w-5 dark:block" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
